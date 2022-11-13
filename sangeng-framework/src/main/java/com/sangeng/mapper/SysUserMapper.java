d848783d0e89b24643a1c3b8a2f5850305ee538c
Alexis <517711766@qq.com>
2022-11-13 13:50:27 +0100

    Config files for Login and JWT

---
+++ sangeng-framework/src/main/java/com/sangeng/mapper/SysUserMapper.java
@@ -0,0 +1,16 @@
+package com.sangeng.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sangeng.domain.entity.SysUser;
+
+
+/**
+ * 用户表(SysUser)表数据库访问层
+ *
+ * @author makejava
+ * @since 2022-11-13 13:38:06
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
+
